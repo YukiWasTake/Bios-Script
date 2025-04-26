c6073506178e0cf1f6dd5ae34abcc49fab14111f
Yuki <jacobhelmick512@gmail.com>
2025-04-25 22:58:57 -0400

    Add files via upload

---
+++ resource/IntelImport.bat
@@ -0,0 +1,28 @@
+@echo off
+:: Request administrator privileges
+fltmc >nul 2>&1 || (
+    PowerShell Start -Verb RunAs '%0' 2> nul || (
+        echo error: right-click on the script and select "Run as administrator"
+        pause
+    )
+    exit /b 1
+)
+
+pushd %~dp0
+
+for %%a in ("amifldrv64.sys", "amigendrv64.sys", "IntelBios.txt") do (
+    if not exist "%%~a" (
+        echo error: %%~a not found in the current directory
+
+        if "%%~a" == "IntelBios.txt" (
+            echo Please rename your NVRAM script file to "IntelBios.txt" or run "Export.bat" to create it
+        )
+
+        pause
+        exit /b 1    
+    )    
+)
+
+SCEWIN_64.exe /I /S IntelBios.txt 2> log-file.txt
+type log-file.txt
+echo See log-file.txt for output messages
